ee65a4aee44e82b1cb4c14fb6025563fac2e4c67
tahfeezmizan <mizanahmed5316@gmail.com>
2025-06-29 19:25:45 +0600

    project is installed, basic structure is complete, and custom fonts  included

---
+++ src/app/(common)/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1 className='font-poppins'>STA Task </h1>
+      <h1 className='font-rancho'>This is a Next.js app</h1>
+    </div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins, Rancho } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const rancho = Rancho({
+  variable: "--font-rancho",
   subsets: ["latin"],
+  weight: ["400"],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} ${rancho.variable} antialiased`}>
         {children}
       </body>
     </html>
